c10c26b7ceb31a472d0ac8ed0135fa989fef91cb
salayhin <salayhin@gmail.com>
2013-09-19 11:46:41 +0600

    Jquery Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 FacebookJs::Application.routes.draw do
   resources :posts
 
+  root  'posts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
